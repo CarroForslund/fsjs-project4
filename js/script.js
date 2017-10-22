2a23f7bf9f257a559362dbf92210de82651db8b4
Caroline Forslund <cf222ev@student.lnu.se>
2017-10-22 22:59:08 +0200

    Result logic started in comment form

--- js/script.js
+++ js/script.js
@@ -112,6 +112,20 @@ function toggleBoxBackground(box){
 
 };
 
+//If these boxes have the same value we have a winner
+  //Horizontal
+    //1, 2, 3
+    //4, 5, 6
+    //7, 8, 9
+  //Vertical
+    //1, 4, 7
+    //2, 5, 8
+    //3, 6, 9
+  //Diagonal
+    //1, 5, 9
+    //3, 5, 7
+//If there isn't a winner it's a draw
+
 function runProgram(){
   hideBoard();
   showStartScreen();
